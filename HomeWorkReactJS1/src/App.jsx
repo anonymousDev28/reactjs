052191b09a5b899c2e3273f7a8411291c95896d0
anonymousDev28 <92519101+anonymousDev28@users.noreply.github.com>
2023-03-10 18:24:23 +0700

    update B1 reactjs

--- HomeWorkReactJS1/src/App.jsx
+++ HomeWorkReactJS1/src/App.jsx
@@ -5,15 +5,34 @@ import "./App.css";
 function App() {
   const [items, setItems] = useState([]);
   const [newItem, setNewItem] = useState("");
+  const [isHidden, setIsHidden] = useState(false);
+  const [hideLabel, setHideLabel] = useState("Hide");
+  //Thêm 1 nút “add” + 1 ô input để nhập text. Mỗi khi bấm nút thêm 1 thẻ <li> có nội dung là nội dung trong ô input vào cuối danh sách
   const handleAddClick = () => {
     if (newItem.trim()) {
       setItems([...items, newItem]);
       setNewItem("");
     }
   };
+  //Thêm 1 nút “remove”. Chức năng để xóa thẻ <li> cuối cùng của danh sách
+  function handleRemove() {
+    setItems((prevItems) => {
+      const newItems = [...prevItems];
+      newItems.pop();
+      return newItems;
+    });
+  }
+  //Thêm 1 nút “Hide” trên đầu của danh sách <ul>.
+  // Khi bấm vào “Hide” thì <ul> sẽ ẩn. Đồng thời label của nút “Hide” => “Show”
+  // Và ngược lại Khi bấm vào “Show” thì <ul> sẽ hiện. Đồng thời label của nút “Show” => “Hide”
+  function handleToggle() {
+    setIsHidden((prevIsHidden) => !prevIsHidden);
+    setHideLabel((prevHideLabel) => (prevHideLabel === "Hide" ? "Show" : "Hide"));
+  }
   return (
     <div className="">
-      <ul id="list">
+      <button onClick={handleToggle}>{hideLabel}</button>
+      <ul id="list" className={isHidden ? "hidden" : ""}>
         {items.map((item, index) => (
           <li key={index}>{item}</li>
         ))}
@@ -24,6 +43,7 @@ function App() {
         onChange={(e) => setNewItem(e.target.value)}
       />
       <button onClick={handleAddClick}>add</button>
+      <button onClick={handleRemove}>Remove</button>
     </div>
   );
 }
